d03c439f65346c83ecd575704111bd69549e7ee7
Saviour14 <bsaviour@icloud.com>
2025-06-02 05:35:36 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,11 +2,11 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '+233553423929', //Your number
+  botOwner: 'Knight Bot', // Your name
+  ownerNumber: '+233554692706', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot to save view once and automating tasks.",
+  description: "This is a bot to save view once and automating tasks,auto status view.",
   version: "1.0.0",
 };
 
